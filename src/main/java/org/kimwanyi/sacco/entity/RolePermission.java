97bee6901071c62274b3b1946d239238a6e0c633
NsubugaCollins <collinsnsubuga.ug@gmail.com>
2026-07-20 13:00:30 +0300

    creating role-permission associative entity

---
+++ src/main/java/org/kimwanyi/sacco/entity/RolePermission.java
@@ -0,0 +1,53 @@
+package org.kimwanyi.sacco.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "role_permission",
+        uniqueConstraints = {
+        @UniqueConstraint(name = "uk_role_permission", columnNames = {
+                "role_id", "permission_id"
+        })
+        }
+)
+public class RolePermission extends BaseEntity{
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinColumn(name = "role_id", nullable = false)
+    private Role role;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinColumn(name = "permission_id", nullable = false)
+    private Permission permission;
+
+    @Column(nullable = false)
+    private boolean active = true;
+
+    public RolePermission(){
+
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public Permission getPermission() {
+        return permission;
+    }
+
+    public void setPermission(Permission permission) {
+        this.permission = permission;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+}
